9ccf8c222defa50ddaace887a182d0c4fa842ee8
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-10-11 00:37:24 -0300

    cadastrando categorias no banco de dados

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category")
+const slugify = require("slugify")
 
 
 router.get("/admin/categories/new", (req,res) => {
     res.render("admin/categories/new")
 });
+
+router.post("/categories/save", (req,res)=>{
+    var title = req.body.title
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect("/")
+        })
+
+    }else{
+        res.redirect("admin/categories/new") //caso seja invalido o nome redireciona
+    }
+})
 
 module.exports = router; 
