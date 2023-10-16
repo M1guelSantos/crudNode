a9a3742176e95cabbb6b4ea1f0f05fcacdca5a5b
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-10-15 23:44:58 -0300

    crud de categorias finalizado

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -16,7 +16,7 @@ router.post("/categories/save", (req,res)=>{
             title: title,
             slug: slugify(title)
         }).then(()=>{
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
 
     }else{
@@ -30,4 +30,56 @@ router.post("/categories/save", (req,res)=>{
         })
     })
 
+    router.post("/categories/delete", (req,res)=>{
+        var id = req.body.id
+        if(id != undefined){
+            if(!isNaN(id)){
+
+                Category.destroy({
+                    where: {
+                        id: id //estou destruindo o id que for igual minha var ID
+                    }
+                }).then(()=>{
+                    res.redirect("/admin/categories")
+                })
+
+            }else{
+                res.redirect("/admin/categories")
+            }
+             
+        }else{
+            res.redirect("/admin/categories") //caso seja invalido o nome redireciona
+        }
+    })    
+
+    router.get("/admin/categories/edit/:id", (req, res) =>{
+        var id = req.params.id
+        if(isNaN(id)){
+            res.redirect("/admin/categories")
+        }
+        Category.findByPk(id).then(category => {
+            if(category != undefined){
+                res.render("admin/categories/edit", {category: category})
+            }else{
+                res.redirect("/admin/categories")
+            }
+        }) .catch(erro =>{
+            res.redirect("/admin/categories")
+        })
+    })
+
+    router.post("/categories/update", (req, res) =>{ 
+        var id = req.body.id
+        var title = req.body.title
+
+        Category.update({title: title, slug: slugify(title)}, {
+            where : {
+                id: id
+            }
+        }).then(()=>{
+            res.redirect("/admin/categories")
+        })
+
+
+    })
 module.exports = router; 
