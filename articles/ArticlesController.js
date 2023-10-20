9b7b75276c41f4987dab48876b0ad5de373b3f8a
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-10-20 00:54:22 -0300

    partes finais crud articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
 
-router.get ("/articles", (req,res) => {
-    res.send("Rota de artigos");
+router.get ("/admin/articles", (req,res) => {
+        Article.findAll({
+            include: [{model: Category}]
+        }).then(articles=>{
+            res.render("admin/articles/index", {articles: articles})
+        })
+    });
+    
+router.get("/admin/articles/new", (req,res) =>{
+    Category.findAll().then(categories=>{
+        res.render("admin/articles/new", {categories: categories})
+    })
+});
+
+router.post("/articles/save", (req,res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var categoria = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: categoria
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
 })
 
-router.get("/admin/articles/new", (req,res) =>{
-    res.render("admin/articles/new")
-});
+router.post("/articles/delete", (req,res)=>{
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){
+
+            Article.destroy({
+                where: {
+                    id: id //estou destruindo o id que for igual minha var ID
+                }
+            }).then(()=>{
+                res.redirect("/admin/articles")
+            })
+
+        }else{
+            res.redirect("/admin/articles")
+        }
+         
+    }else{
+        res.redirect("/admin/articles") //caso seja invalido o nome redireciona
+    }
+})    
+
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize")
 
 const connection = new Sequelize('guiapress', 'root', 'miguel123', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
